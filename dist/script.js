67ea3e133560be84dab3c4b73c2b11ccc0a8fb1a
Kkasya <kasya--k@yandex.by>
2020-11-24 18:48:56 +0300

    feat: add airbnb

--- dist/script.js
+++ dist/script.js
@@ -5,6 +5,9 @@
 /*! unknown exports (runtime-defined) */
 /*! runtime requirements:  */
 console.log('hi');
+var b = 5;
+var a = b++;
+console.log(a);
 /******/ })()
 ;
 //# sourceMappingURL=script.js.map
--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
 console.log('hi');
+let b = 5;
+const a = b++;
+console.log(a);
